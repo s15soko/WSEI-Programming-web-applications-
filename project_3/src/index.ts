496c87a18a52a5d6fdaaabcc65492e13bab300df
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-04-11 22:14:21 +0200

    Events for weather

--- project_3/src/index.ts
+++ project_3/src/index.ts
@@ -2,6 +2,7 @@ import App from "./App";
 import { WeatherCardBuilder } from "./WeatherCardBuilder";
 
 const app = new App();
+const currentItems = app.getItems();
 const result = document.getElementById("result");
 const searchBtn = document.getElementById("search");
 const searchInput = document.getElementById("searchInput") as HTMLInputElement;
@@ -13,8 +14,24 @@ const getCity = () => {
 searchBtn?.addEventListener("click", async function() {
     const cityName = getCity();
     const city = await app.getCityInfo(cityName);
-    if("weather" in city) {
+
+    if("weather" in city && !currentItems.includes(cityName)) 
+    {
+        app.setOrSkip(cityName);
         const builder = new WeatherCardBuilder(city);
         result?.append(builder.build());
     }
 });
+
+window.addEventListener("load", function() {
+    app.getItems().forEach(async (item: string) => {
+        const city = await app.getCityInfo(item);
+        
+        if("weather" in city)
+        {
+            console.log(city)
+            const builder = new WeatherCardBuilder(city);
+            result?.append(builder.build());
+        }
+    });
+})
